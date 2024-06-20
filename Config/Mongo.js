75c65b2aa708aaa70c704a5ca993085ff6e4e34b
RamadeviRamkumar <ramdevcse25@gmail.com>
2024-06-20 12:04:45 +0530

    new

--- Config/Mongo.js
+++ Config/Mongo.js
@@ -1,4 +1,5 @@
-module.exports = { 'url' : 'mongodb://127.0.0.1:/EMS',
+module.exports = { 'url' : 'mongodb+srv://ramdevcse25:rama123@employee-management-sys.uaragq4.mongodb.net/EMS',
+    'url1' : 'mongodb://127.0.0.1:/EMS',
 };
 PORT = 8000
 
